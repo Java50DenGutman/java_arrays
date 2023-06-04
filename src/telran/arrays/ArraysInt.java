a338cd0d0c6a8a1f68ec11febbb57fd4b8eb5a7b
Den Gutman <dengutman@gmail.ru>
2023-06-04 22:57:29 +0300

    HW #03

--- src/telran/arrays/ArraysInt.java
+++ src/telran/arrays/ArraysInt.java
@@ -1,48 +1,118 @@
 package telran.arrays;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class ArraysInt {
-public static int[] addNumber(int [] array, int number) {
-	int [] res = Arrays.copyOf(array, array.length+1);
-	res[array.length] = number;
-	return res;
-}
-public static int[] insertNumber(int[] array, int index, int number) {
-	int [] res = new int[array.length+1];
-	System.arraycopy(array, 0, res, 0, index);
-	res[index] = number;
-	System.arraycopy(array, index, res, index+1, array.length - index);
-	return res;
-}
-public static int[] removeNumber(int [] array, int number) {
-	int count = 0;
-    for (int i = 0; i < array.length; i++) {
-        if (array[i] != number) {
-            count++;
-        }
-    }
+	public static int[] addNumber(int[] array, int number) {
+		int[] res = Arrays.copyOf(array, array.length + 1);
+		res[array.length] = number;
+		return res;
+	}
 
-    int[] res = new int[count];
-    int j = 0;
-    for (int i = 0; i < array.length; i++) {
-        if (array[i] != number) {
-            res[j] = array[i];
-            j++;
-        }
-    }
+	public static int[] insertNumber(int[] array, int index, int number) {
+		int[] res = new int[array.length + 1];
+		System.arraycopy(array, 0, res, 0, index);
+		res[index] = number;
+		System.arraycopy(array, index, res, index + 1, array.length - index);
+		return res;
+	}
 
-	return res;
-}
-public static int[] insertNumberSorted(int [] arraySorted, int number) {
-	int index = Arrays.binarySearch(arraySorted, number);
-    if (index < 0) {
-        index = -(index + 1);
-    }
-    int[] res = new int[arraySorted.length + 1];
-    System.arraycopy(arraySorted, 0, res, 0, index);
-    res[index] = number;
-    System.arraycopy(arraySorted, index, res, index + 1, arraySorted.length - index);
-    return res;
-}
+	public static int[] removeNumber(int[] array, int index) {
+		// returns array with no number at the given index
+		int[] res = new int[array.length - 1];
+		System.arraycopy(array, 0, res, 0, index);
+		System.arraycopy(array, index + 1, res, index, res.length - index);
+		return res;
+	}
+
+	public static int[] insertNumberSorted(int[] arraySorted, int number) {
+		// takes sorted array and number to insert
+		// returns sorted array with the given number at a proper position
+		// apply the standard method of the class Arrays int binarySearch(int[]
+		// arraySorted, int key)
+		int index = Arrays.binarySearch(arraySorted, number);
+		if (index < 0) {
+			index = -(index + 1);
+		}
+		return insertNumber(arraySorted, index, number);
+	}
+
+	public static int searchNumber(int[] array, int number) {
+		// searching for the given number in the given array
+		// returns index value for the number if exists otherwise false
+		int index = 0;
+		while (index < array.length && array[index] != number) {
+			index++;
+		}
+		return index < array.length ? index : -1;
+	}
+
+	public static int binarySearch(int[] array, int number) {
+		int left = 0;
+		int right = array.length - 1;
+		int middle;
+		int result = -1;
+
+		while (left <= right) {
+			middle = left + (right - left) / 2;
+
+			if (array[middle] == number) {
+				result = middle;
+				right = middle - 1;
+			} else if (array[middle] < number) {
+				left = middle + 1;
+			} else {
+				right = middle - 1;
+			}
+		}
+
+		if (result == -1) {
+			return -(left + 1);
+		} else {
+			return result;
+		}
+	}
+
+	public static void quickSort(int[] array) {
+		Arrays.sort(array);
+	}
+
+	public static void bubbleSort(int[] array) {
+		int length = array.length;
+		boolean flSorted = false;
+		do {
+			flSorted = true;
+			length--;
+			for (int i = 0; i < length; i++) {
+				if (array[i] > array[i + 1]) {
+					swap(array, i, i + 1);
+					flSorted = false;
+				}
+			}
+		} while (!flSorted);
+	}
+
+	private static void swap(int[] array, int i, int j) {
+		// ONLY for interview task of swapping with no additional variable
+		// array[i] = array[i] + array[j];
+		// array[j] = array[i] - array[j];
+		// array[i] = array[i] - array[j];
+		//
+		int tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+
+	}
+
+	public static int[] generateRandomArray(int length) {
+		int[] array = new int[length];
+		Random random = new Random();
+
+		for (int i = 0; i < length; i++) {
+			array[i] = random.nextInt();
+		}
+
+		return array;
+	}
 }
--- src/telran/arrays/test/ArraysTest.java
+++ src/telran/arrays/test/ArraysTest.java
@@ -1,99 +1,169 @@
 package telran.arrays.test;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import telran.arrays.ArraysInt;
+import telran.arrays.ArraysInt.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
+
 public class ArraysTest {
-@Test
-void initialTests() {
-	int [] ar1 = {1,2,3};
-	int [] ar2 = {1,2,3};
-	int [] ar2_3 = ar2;
-	assertFalse(ar1==ar2);
-	assertTrue(ar2==ar2_3);
-	assertArrayEquals(ar1, ar2);
-	ar2_3[0]=10;
-	ar1[0]=10;
-	assertArrayEquals(ar1, ar2); // wrong test
-	
-}
-@Test
-void copyOfIntTest() {
-	int [] ar1 = {10,5,15};
-	int [] expected1 = {10,5,15};
-	int [] expected2 = {10,5};
-	int [] expected3 = {10,5,15,0,0};
-	assertArrayEquals(expected1, Arrays.copyOf(ar1, 3));
-	assertArrayEquals(expected2, Arrays.copyOf(ar1, 2));
-	assertArrayEquals(expected3, Arrays.copyOf(ar1, 5));
-}
-@Test
-void addNumberTest() {
-	int [] ar1 = {10,5,15};
-	int number = 20;
-	int [] expected = {10,5,15,number};
-	assertArrayEquals(expected, ArraysInt.addNumber(ar1, number));
-}
-@Test
-void arraycopyTest() {
-	int [] src = {1,2,3,4,5,6,7};
-	int [] dest = {10,20,30,40,50,60,70};
-	int [] expected = {10,20,30,40,1,2,3};
-	System.arraycopy(src, 0, dest, 4, 3);
-	assertArrayEquals(expected, dest);
-}
-@Test
-void insertNumberTest() {
-	int [] src = {1,2,3,4,5,6,7};
-	int number = 20;
-	int index = 3;
-	int [] expected1 = {1,2,3,20,4,5,6,7};
-	assertArrayEquals(expected1, ArraysInt.insertNumber(src, index, number));
-	number = 30;
-	index = 0;
-	int [] expected2 = {30,1,2,3,4,5,6,7};
-	assertArrayEquals(expected2, ArraysInt.insertNumber(src, index, number));
-	number = 40;
-	index = 7;
-	int [] expected3 = {1,2,3,4,5,6,7,40};
-	assertArrayEquals(expected3, ArraysInt.insertNumber(src, index, number));
+	@Test
+	void initialTest() {
+		int[] ar1 = { 1, 2, 3 };
+		int[] ar2 = { 1, 2, 3 };
+		int[] ar2_3 = ar2;
+		assertFalse(ar1 == ar2);
+		assertTrue(ar2 == ar2_3);
+		assertArrayEquals(ar1, ar2);
+		ar2_3[0] = 10;
+		ar1[0] = 10;
+		assertArrayEquals(ar1, ar2);
 
+	}
 
-}
-@Test
-void removeNumberTest() {
-	 int[] src = {1, 2, 3, 4, 5, 6, 7};
-	 int number = 3;
-	 int[] expected = {1, 2, 4, 5, 6, 7};
-	 assertArrayEquals(expected, ArraysInt.removeNumber(src, number));
-}
-@Test
-void binarySearchTest() {
-	int[] arr = {1, 2, 4, 7, 9, 11};
-    int key1 = 4;
-    int key2 = 8;
-    int key3 = 1;
+	@Test
+	void copyOfIntTest() {
+		int[] ar1 = { 10, 5, 15 };
+		int[] expected1 = { 10, 5, 15 };
+		int[] expected2 = { 10, 5 };
+		int[] expected3 = { 10, 5, 15, 0, 0 };
+		assertArrayEquals(expected1, Arrays.copyOf(ar1, 3));
+		assertArrayEquals(expected2, Arrays.copyOf(ar1, 2));
+		assertArrayEquals(expected3, Arrays.copyOf(ar1, 5));
+	}
 
-    assertEquals(2, Arrays.binarySearch(arr, key1));
-    assertTrue(Arrays.binarySearch(arr, key2) < 0);
-    assertEquals(0, Arrays.binarySearch(arr, key3));
-}
-@Test
-void insertSortedTest() {
-    int[] arr = {2, 4, 6, 8, 10};
-    int number1 = 5;
-    int number2 = 11;
-    int number3 = 6;
-    int[] expected1 = {2, 4, 5, 6, 8, 10};
-    int[] expected2 = {2, 4, 6, 8, 10, 11};
-    int[] expected3 = {2, 4, 6, 6, 8, 10};
+	@Test
+	void addNumberTest() {
+		int[] ar1 = { 10, 5, 15 };
+		int number = 20;
+		int[] expected = { 10, 5, 15, number };
+		assertArrayEquals(expected, ArraysInt.addNumber(ar1, number));
+	}
 
-    assertArrayEquals(expected1, ArraysInt.insertNumberSorted(arr, number1));
-    assertArrayEquals(expected2, ArraysInt.insertNumberSorted(arr, number2));
-    assertArrayEquals(expected3, ArraysInt.insertNumberSorted(arr, number3));
-}
+	@Test
+	void arraycopyTest() {
+		int[] src = { 1, 2, 3, 4, 5, 6, 7 };
+		int[] dest = { 10, 20, 30, 40, 50, 60, 70 };
+		int[] expected = { 10, 20, 30, 40, 1, 2, 3 };
+		System.arraycopy(src, 0, dest, 4, 3);
+		assertArrayEquals(expected, dest);
+	}
+
+	@Test
+	void insertNumberTest() {
+		int[] src = { 1, 2, 3, 4, 5, 6, 7 };
+		int number = 20;
+		int index = 3;
+		int[] expected1 = { 1, 2, 3, 20, 4, 5, 6, 7 };
+		assertArrayEquals(expected1, ArraysInt.insertNumber(src, index, number));
+		number = 30;
+		index = 0;
+		int[] expected2 = { 30, 1, 2, 3, 4, 5, 6, 7 };
+		assertArrayEquals(expected2, ArraysInt.insertNumber(src, index, number));
+		number = 40;
+		index = 7;
+		int[] expected3 = { 1, 2, 3, 4, 5, 6, 7, 40 };
+		assertArrayEquals(expected3, ArraysInt.insertNumber(src, index, number));
+	}
+
+	@Test
+	void removeNumberTest() {
+		int[] src = { 1, 2, 3, 4, 5, 6, 7 };
+		int index = 3;
+		int[] expected1 = { 1, 2, 3, 5, 6, 7 };
+		assertArrayEquals(expected1, ArraysInt.removeNumber(src, index));
+		index = 0;
+		int[] expected2 = { 2, 3, 4, 5, 6, 7 };
+		assertArrayEquals(expected2, ArraysInt.removeNumber(src, index));
+		index = 4;
+		int[] expected3 = { 1, 2, 3, 4, 6, 7 };
+		assertArrayEquals(expected3, ArraysInt.removeNumber(src, index));
+	}
+
+	@Test
+	void binarySearchTest() {
+
+		// int key)
+		int[] src = { 10, 20, 30, 40, 50, 60, 70 };
+		assertEquals(3, Arrays.binarySearch(src, 40));
+		assertEquals(0, Arrays.binarySearch(src, 10));
+		assertEquals(6, Arrays.binarySearch(src, 70));
+		assertEquals(-1, Arrays.binarySearch(src, 5));
+		assertEquals(-4, Arrays.binarySearch(src, 35));
+		assertEquals(-8, Arrays.binarySearch(src, 75));
+	}
+
+	@Test
+	void insertSortedTest() {
+		int[] src = { 10, 20, 30, 40, 50, 60, 70 };
+		int[] expected1 = { 10, 20, 30, 40, 40, 50, 60, 70 };
+		int[] expected2 = { 10, 10, 20, 30, 40, 50, 60, 70 };
+		int[] expected3 = { 10, 20, 30, 40, 50, 60, 70, 70 };
+		int[] expected4 = { 5, 10, 20, 30, 40, 50, 60, 70 };
+		int[] expected5 = { 10, 20, 30, 35, 40, 50, 60, 70 };
+		int[] expected6 = { 10, 20, 30, 40, 50, 60, 70, 75 };
+
+		assertArrayEquals(expected1, ArraysInt.insertNumberSorted(src, 40));
+		assertArrayEquals(expected2, ArraysInt.insertNumberSorted(src, 10));
+		assertArrayEquals(expected3, ArraysInt.insertNumberSorted(src, 70));
+		assertArrayEquals(expected4, ArraysInt.insertNumberSorted(src, 5));
+		assertArrayEquals(expected5, ArraysInt.insertNumberSorted(src, 35));
+		assertArrayEquals(expected6, ArraysInt.insertNumberSorted(src, 75));
+	}
+
+	@Test
+	void maxIntTest() {
+		assertEquals(Integer.MAX_VALUE, getIntMax());
+	}
+
+	private int getIntMax() {
+		int res = 1;
+		while (res > 0) {
+			res++;
+		}
+
+		return res - 1;
+	}
+
+	@Test
+	@Timeout(3)
+	void maxLongTest() {
+		assertEquals(Long.MAX_VALUE, getLongMax());
+	}
+
+	private long getLongMax() {
+		long res = 1;
+		while (res > 0) {
+			res *= 2;
+		}
+
+		return res - 1;
+	}
+
+	@Test
+	void muchRepeatedTest() {
+		int N_ELEMENTS = 1000000;
+		int N_RUNS = 1000000;
+		int[] array = new int[N_ELEMENTS];
+
+		for (int i = 0; i < N_RUNS; i++) {
+			assertEquals(0, ArraysInt.binarySearch(array, 0));
+		}
+	}
+
+	@Test
+	void sortingComparisonTest() {
+		int N_ELEMENTS = 100000;
+		int[] array = ArraysInt.generateRandomArray(N_ELEMENTS);
+		int[] copy1 = Arrays.copyOf(array, array.length);
+		int[] copy2 = Arrays.copyOf(array, array.length);
+		ArraysInt.quickSort(copy1);
+		ArraysInt.bubbleSort(copy2);
+		assertArrayEquals(copy1, copy2);
+	}
+
 }
